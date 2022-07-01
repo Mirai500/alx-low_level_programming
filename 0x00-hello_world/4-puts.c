9bb262ce44fd8d3c136ea8dd76477485ae0269d8
Mirai500 <asifatumahbub001@gmail.com>
2022-07-01 03:53:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - main block
- * return 0
+ * Return: 0
  */
 
-int main()
+int main(void)
 {
-	puts("Programming ia like building a multilingual puzzle");
+	puts("\"Programming ia like building a multilingual puzzle");
 	return (0);
 }
