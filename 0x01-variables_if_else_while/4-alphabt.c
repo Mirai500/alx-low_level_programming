9a322d29aca0b757aff9214fbec0cda2a7840d13
Mirai500 <asifatumahbub001@gmail.com>
2022-07-03 20:26:42 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		while (i != e && i != q){
+
+		putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
