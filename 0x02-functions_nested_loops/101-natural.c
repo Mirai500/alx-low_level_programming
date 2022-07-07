7acfa3b718c2fd3429deba00c72990f72dd1c2d5
Mirai500 <asifatumahbub001@gmail.com>
2022-07-06 20:39:10 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include 
-stdlib.h>
+#include <stdlib.h>
 
 /**
  * main - main block
  * Descri: ion: computes and print the sum of all the multiples of 3 or
- * 5 below 1024(excluded), followed by a new line
+ * 5 belo 1024(excluded), followed by a new line
  * Return : 0
  */
 int main(void)
