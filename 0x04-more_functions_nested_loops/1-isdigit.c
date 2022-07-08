b2bfe72a71818f3893923fdf68491aaf0fa1e72f
Mirai500 <asifatumahbub001@gmail.com>
2022-07-08 10:20:29 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks if parameter is a digit
+ * c - parameter to be checked
+ * Return: 1 if c is a digit, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
