d30343455bdd018a1ac4c075e60f2daec0c49d34
Mirai500 <asifatumahbub001@gmail.com>
2022-07-08 20:52:34 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - main block
- * Descri: ion: computes and print the sum of all the multiples of 3 or
- * 5 belo 1024(excluded), followed by a new line
- * Return : 0
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int c = 0;
-	int sum = 0;
+	int a, b;
 
-	while (c < 1024)
+	for (a = 1; a < 1024; a++)
 	{
-		if (c % 3 == 0 || c % 5 == 0)
-		{
-			sum += c;
-		}
-
-		c++;
-
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", b);
 	return (0);
 }
