645b26e456c8ab9e2dda3ed968e1ca661a432e84
Mirai500 <asifatumahbub001@gmail.com>
2022-07-12 00:56:35 -0700

    0-reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,15 +6,7 @@
  * ptr_n: pointer to be updated
  * Return: Always 0.
  */
-void reset_to_98(int n)
+void reset_to_98(int *n)
 {
-	int n, *ptr_n;
-
-	int n;
-
-	n = 402;
-
-	*ptr_n = &n;
-
-	*ptr_n = 98;
+	*n = 98;
 }
