df9e837ca8023a62958e2709d8890c2f2a4259b7
Mirai500 <asifatumahbub001@gmail.com>
2022-07-12 02:56:52 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string whose length is to be returned
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	return strlen( *s );
+}
