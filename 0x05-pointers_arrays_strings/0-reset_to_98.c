2d3bd595606469cd8af41e3d550902052882b7fe
Mirai500 <asifatumahbub001@gmail.com>
2022-07-12 01:01:28 -0700

    0-reset-to-98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,8 @@
 
 /**
  * reset_to_98 - updates the value of a pointer to 98
- * n: integer
- * ptr_n: pointer to be updated
- * Return: Always 0.
+ * @n: integer
+ * Return: no return.
  */
 void reset_to_98(int *n)
 {
